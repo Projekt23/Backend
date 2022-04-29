4a546f3c1f5e0bfe817f6db0df740407930d2545
Simon <simon.geissler@kiongroup.com>
2022-04-29 11:49:12 +0200

    Init

---
+++ src/main/java/com/project23/app/Project23Application.java
@@ -0,0 +1,13 @@
+package com.project23.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project23Application.class, args);
+    }
+
+}
