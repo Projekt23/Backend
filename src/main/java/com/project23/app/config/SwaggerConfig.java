0cb43f6fd5e5efff1ce736821c1826348df998aa
sig268 <s.r.geissler@gmail.com>
2022-05-05 14:49:44 +0200

    - Added SwaggerConfig

---
+++ src/main/java/com/project23/app/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.project23.app.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    /**
+     * Default Api Dokumentations- und Test-Seite
+     * @return OpenApi Object
+     */
+    @Bean
+    public GroupedOpenApi Api() {
+        return GroupedOpenApi.builder()
+                .group("api")
+                .pathsToMatch("/*")
+                .build();
+    }
+    /**
+     * User Api Dokumentations- und Test-Seite
+     * @return OpenApi Object
+     */
+    @Bean
+    public GroupedOpenApi userApi() {
+        return GroupedOpenApi.builder()
+                .group("user-api")
+                .pathsToMatch("/api/user/*")
+                .build();
+    }
+}
