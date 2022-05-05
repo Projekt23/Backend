15111607deaa534632a0a50464af761c6adacfe0
sig268 <s.r.geissler@gmail.com>
2022-05-05 10:51:57 +0200

    - Configured Database - Added new Configuration for Stages

--- src/main/java/com/project23/app/Project23.java
+++ src/main/java/com/project23/app/Project23.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Project23Application {
+public class Project23 {
 
     public static void main(String[] args) {
-        SpringApplication.run(Project23Application.class, args);
+        SpringApplication.run(Project23.class, args);
     }
 
 }
